1b39c58c6833726710967209c9c98d213f5e736f
Brian Mathews <brian@wintr.us>
2016-06-01 17:44:18 -0700

    WIP menu icon, currently hidden

--- client/app.js
+++ client/app.js
@@ -5,7 +5,6 @@ import {
 
 var React = require('react');
 var Icon = require('./components/icon');
-var Toolbar = require('./components/toolbar');
 var Calendar = require('./components/calendar/calendar');
 var EventList = require('./components/event-list');
 
@@ -14,7 +13,7 @@ export default React.createClass({
   getInitialState () {
     return {
       events: [],
-      view: 'week'
+      view: 'month'
     };
   },
 
@@ -73,7 +72,9 @@ export default React.createClass({
   render () {
     return (
       <div className="flex-column">
-        <Toolbar key="toolbar" profile={this.state.profile}/>
+        <div className="menu-icon">
+          <Icon icon="menu"/>
+        </div>
         <Calendar ref="calendar" key="calendar" view={this.state.view} events={this.state.events} selectedDate={new Date()} onChange={this._handleCalendarSelect}/>
         <EventList ref="eventlist" key="events" onHeaderClick={this._handleHeaderClick} onEventClick={this._handleEventClick} events={this.state.events}/>
       </div>
